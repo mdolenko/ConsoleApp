6d0884f70eced7a26ad0a4211d7f18de2a1ab5cb
DESKTOP-EJRL0B4\mdolenko <mishDolenko@gmail.com>
2017-12-17 22:34:08 +0200

    Initial commit to GitHub

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, string, int> qwer = (q, e) => {
+                return 1;
+            };
+
+            Console.WriteLine(qwer.Invoke(1, "test"));
+
+            Console.Read();
+        }
+
+        static void Operation(int x1, int x2, Action<int, int> op)
+        {
+            if (x1 > x2)
+                op(x1, x2);
+        }
+
+        static void Add(int x1, int x2)
+        {
+            Console.WriteLine("Сумма чисел: " + (x1 + x2));
+        }
+
+        static void Substract(int x1, int x2)
+        {
+            Console.WriteLine("Разность чисел: " + (x1 - x2));
+        }
+    }
+}
